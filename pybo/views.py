4df59949b8402bf2df6f199866af859b5dd6b0de
Heejun Shin <abel9851@gmail.com>
2021-07-14 17:19:50 +0900

    create Answer function

--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import Question
+from django.utils import timezone
 
 
 def index(request):  # request는 장고에 의해 자동으로 전달되는 HTTP요청 객체이다.
@@ -18,7 +19,18 @@ def detail(request, question_id):
     pybo 내용 출력
     """
     question = get_object_or_404(
-        Question, id=question_id
+        Question, pk=question_id
     )  # 키워드 인자는 매개변수도 키워드인자의 키워드와 이름이 같아야한다.
     context = {"question": question}
     return render(request, "pybo/question_detail.html", context)
+
+
+def answer_create(request, question_id):
+    """
+    pybo 답변 등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(
+        content=request.POST.get("content"), create_date=timezone.now()
+    )
+    return redirect("pybo:detail", question_id=question_id)
