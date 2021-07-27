cc7744fc17bd989dc22735203646d61e1af4130e
Heejun Shin <abel9851@gmail.com>
2021-07-27 13:38:20 +0900

    create search function

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,4 +1,5 @@
 from django.core.paginator import Paginator
+from django.db.models import Q, Count  # 조회용 Query 객체 생성
 from django.shortcuts import render, get_object_or_404
 
 from ..models import Question
@@ -11,13 +12,35 @@ def index(request):  # request는 장고에 의해 자동으로 전달되는 HTT
     pybo 목록 출력
     """
     page = request.GET.get("page", "1")  # 페이지
+    kw = request.GET.get("kw", "")  # 검색어
+    so = request.GET.get("so", "recent")  # 정렬기준
 
-    question_list = Question.objects.order_by("-create_date")
+    # 정렬
+    if so == "recommend":
+        question_list = Question.objects.annotate(num_voter=Count("voter")).order_by(
+            "-num_voter", "-create_date"
+        )
+    elif so == "popular":
+        question_list = Question.objects.annotate(num_answer=Count("answer")).order_by(
+            "-num_answer", "-create_date"
+        )
+    else:  # recent
+        question_list = Question.objects.order_by("-create_date")
+    # 검색
 
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw)  # 제목 검색  icontains는 대,소문자 상관없이 안에 키워드를 포함하는 객체를 반환
+            | Q(content__icontains=kw)  # 내용 검색
+            | Q(author__username__icontains=kw)  # 질문 글쓴이 검색
+            | Q(answer__author__username__icontains=kw)  # 답변 글쓴이 검색
+        ).distinct()
+
+    # 페이징 처리
     paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
 
-    context = {"question_list": page_obj}
+    context = {"question_list": page_obj, "page": page, "kw": kw, "so": so}
     return render(request, "pybo/question_list.html", context)
 
 
