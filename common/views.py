6b404cc782076830bf50b51cc9ca3038ede6b617
Heejun Shin <abel9851@gmail.com>
2021-07-30 15:19:27 +0900

    add exception information in 404 page

--- common/views.py
+++ common/views.py
@@ -25,4 +25,4 @@ def page_not_found(request, exception):
     """
     404 Page not found
     """
-    return render(request, "common/404.html", {})
+    return render(request, "common/404.html", {"exception": exception})
